5649a7e79a283353330004f8e004e5f45d86cef9
Still34 <Still34@users.noreply.github.com>
2022-10-23 00:33:12 +0000

    jekyll build from Action a71386224dc88ecdd115729a88af78193e25ce79

---
+++ README.md
@@ -0,0 +1,11 @@
+# Alrest Wiki
+
+This repository contains the Jekyll page behind the https://alrest.wiki, an all-in-one Xeno-series technical reverse engineering documentation. All of the pages are automatically built and served to the `gh-pages` branch via a fork of [jekyll-action](https://github.com/Still34/jekyll-action).
+
+## Contribution
+
+Any kind of help is wanted, as this is a massive project and undertaking for a team with less than a few members. If you had previously worked on reverse engineering any of the Xeno games from MonolithSoft, please do chip in! Anyone can open a PR to create and/or modify an existing page, though preferably an issue should be opened beforehand. If you have no idea how to open a PR, simply open an issue stating the changes you'd like, and after discussion, we will integrate it into our pages.
+
+## Credits
+
+See [About](https://alrest.wiki/about).
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Get Started with Xenoblade Reverse Engineering",
+        "excerpt":"Which Xeno-series would you like to start with?   ","categories": [],
+        "tags": [],
+        "url": "/get-started",
+        "teaser": null
+      },{
+        "title": "File Types in the Xenoblade Series",
+        "excerpt":"This page contains all of the proprietary file types found across all of the Xenoblade series. All-purpose ARH Header: arh1 (61 72 68 31) Description: Proprietary header file. Contains information for the corresponding ARD file. This includes header files of each object and how they are stored in the ARD...","categories": ["xenoblade-general"],
+        "tags": ["Resources","Xenoblade (Series)"],
+        "url": "/files",
+        "teaser": null
+      },{
+        "title": "Xenoblade 3 Resource Links",
+        "excerpt":"Reyn, you’re a lifesafer! This page serves as a master page for links to third-party Xenoblade-3-related resources. Tools XenoLib A C++-based Xenoblade format parser and converter. This library is able to handle most files from Xenoblade 3, including unpacking ARD files, model and texture dumping, and more. Noesis Noesis is...","categories": ["Xenoblade3"],
+        "tags": ["Resources","Xenoblade 3","Models","Textures","Voice lines"],
+        "url": "/xenoblade3/resources/",
+        "teaser": null
+      },{
+        "title": "Hashes of Known Xeno Games",
+        "excerpt":"This page contains hashes of the relevant game executable and dumps. This information may be used to validate your dump of the game. This page should be a direct parity to the dumps found on No-Intro and Redump, along with additional hashes such as their main executables if applicable. Xenogears...","categories": ["xeno-general"],
+        "tags": ["Resources","Xenoblade (Series)","Xenogears","Xenosaga (Series)"],
+        "url": "/hashes",
+        "teaser": null
+      },{
+        "title": "Setting up Nintendo Switch for Remote Debugging (IDA Pro)",
+        "excerpt":"Think you can take me!? This guide assumes the reader has decent knowledge of reverse engineering and low-level debugging and requires the use of a modded Nintendo Switch. What You Need The latest version of IDA Pro The latest version of Atmosphere Instructions Section I - Enable Switch GDB Stub...","categories": ["nx-general"],
+        "tags": ["Resources","Nintendo Switch","Debugging","Reverse engineering"],
+        "url": "/nx-debug-setup-ida-pro",
+        "teaser": null
+      }]
